2aca9e263dc42739472b0790774f6ebb709ab8b4
Renzo Carpio Ponce <me@renzocarpio.com>
2016-08-28 20:12:48 -0500

    migracion de la table profile

--- app/database/migrations/2016_08_29_010602_create_profile_table.php
+++ app/database/migrations/2016_08_29_010602_create_profile_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStatusTable extends Migration {
+class CreateProfileTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreateStatusTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('status', function(Blueprint $table) {
+		Schema::create('profiles', function(Blueprint $table) {
 			$table->increments('id');
             $table->string('description');
 			$table->timestamps();
@@ -26,7 +26,7 @@ class CreateStatusTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('status');
+		Schema::drop('profiles');
 	}
 
 }
